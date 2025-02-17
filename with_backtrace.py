a7b9a4a4dc77932c5b3e1ceeb97f736f5c03b06c
natsu <alaoui.hamza2002@gmail.com>
2025-02-17 18:14:33 +0100

    backtrace fully implemented with bugs fixed and added operations cost and priority

--- with_backtrace.py
+++ with_backtrace.py
@@ -11,8 +11,8 @@ class min_dist:
         self.back_trc = []
         
         for i in range(max(m,n)+1):
-            if m+1 > i : self.dist_mtrx[0][i] = i
-            if n +1 > i : self.dist_mtrx[i][0] = i
+            if m + 1 > i : self.dist_mtrx[0][i] = i
+            if n + 1 > i : self.dist_mtrx[i][0] = i
 
         for i in range(1,n+1):
             for j in range(1,m+1):
@@ -20,9 +20,10 @@ class min_dist:
                     self.dist_mtrx[i][j] = self.dist_mtrx[i-1][j-1]
                 else:
                     a = self.dist_mtrx[i-1][j-1] + self.rep_cost                    
-                    b = self.dist_mtrx[i][j-1] + self.del_cost
-                    c = self.dist_mtrx[i-1][j] + self.ins_cost
+                    b = self.dist_mtrx[i][j-1] + self.ins_cost
+                    c = self.dist_mtrx[i-1][j] + self.del_cost
                     self.dist_mtrx[i][j] = min(a,b,c)
+        print(f"replace: {self.rep_cost}\t insert: {self.ins_cost}\tdelete: {self.del_cost}")
 
     def dist(self):
         return self.dist_mtrx[-1][-1]
@@ -31,14 +32,10 @@ class min_dist:
     def backtrace(self):
         i, j = len(self.str2), len(self.str1)
 
-
-
-        
-
         def add_operation(operation,place):
-            i, j = place
-            x = "" if i == -1 else self.str2[i] 
-            y = "" if j == -1 else self.str2[j]
+            m, n = place
+            x = "" if m == -1 else self.str2[m] 
+            y = "" if n == -1 else self.str1[n]
 
             self.back_trc.append({
                 "operation": operation, 
@@ -54,8 +51,6 @@ class min_dist:
             c = self.dist_mtrx[i-1][j] if i > 0 else float('inf')
             
 
-
-
             operations = [
                     {"cond": i > 0 and x == c + self.del_cost, "oper": "delete", "cost":self.del_cost},
                     {"cond": j > 0 and x == b + self.ins_cost, "oper": "insert", "cost":self.ins_cost},
@@ -67,8 +62,6 @@ class min_dist:
 
             # sort on cost of operations (overwrite the previous sort. cost >> priority)
             operations = sorted(operations, key = lambda op: op["cost"])
-
-            
             
             # the keep operation costs nothing so it's always of high priority 
             if i > 0 and j > 0 and self.str2[i-1] == self.str1[j-1]:
@@ -95,27 +88,22 @@ class min_dist:
                                 i -= 1
                                 j -= 1
                                 break
-
-
-
-            # # update the conditions
-            # for i in len(operations):
-            #     if operations[i]["oper"] == "delete":
-            #         operations[i]["cond"] = j > 0 and x == b + self.del_cost
-            #     elif operations[i]["oper"] == "insert":
-            #         operations[i]["cond"] = j > 0 and x == b + self.ins_cost
-            #     elif operations[i]["oper"] == "replace":
-            #         operations[i]["cond"] = i > 0 and j > 0 and x == a + self.rep_cost
-                
-            
-        # print(cost)
+        # print(f"replace: {self.rep_cost}\t insert: {self.ins_cost}\tdelete: {self.del_cost}")
         return self.back_trc[::-1]  # Reverse
 
     
 
 source = "almma"
 target = "amza"
-min_d = min_dist(source, target, del_cost=20,priority={"delete": 0, "insert": 1, "replace": 3}) #, del_cost=1, ins_cost=1
+min_d = min_dist(source, 
+                 target,
+                #   ins_cost=1, 
+                #   del_cost=1, 
+                  rep_cost=1,
+                  priority={"delete": 1,
+                             "insert": 0, 
+                             "replace": 3}
+                  ) 
 
 
 print(f"source: {source}\t target: {target}")
@@ -124,67 +112,3 @@ for i in min_d.dist_mtrx:
 print(min_d.dist())
 for i in min_d.backtrace():
     print(i)
-
-
-
-
-    # def backtrace(self):
-    #     i, j = len(self.str2), len(self.str1)
-
-    #     while i > 0 or j > 0 :
-    #         x = self.dist_mtrx[i][j]
-    #         a = self.dist_mtrx[i-1][j-1]
-    #         b = self.dist_mtrx[i][j-1]
-    #         c = self.dist_mtrx[i-1][j]
-
-    #         if self.str2[i-1] == self.str1[j-1] :
-    #             self.back_trc.append({
-    #                 "operation":"keep", 
-    #                 "letters": (self.str2[i-1], self.str1[j-1])})
-    #             i -= 1
-    #             j -= 1
-
-    #         else :
-
-    #             if x == b + 1 : 
-    #                 self.back_trc.append({
-    #                     "operation":"delete", 
-    #                     "letters": (self.str2[i-1], "")})
-
-    #                 i -= 1
-                
-    #             elif x == c + 1 :
-    #                 self.back_trc.append({
-    #                     "operation":"insert", 
-    #                     "letters": ("", self.str1[j-1])})
-    #                 j -= 1
-                
-    #             else :
-    #                 self.back_trc.append({
-    #                     "operation":"replace", 
-    #                     "letters": (self.str2[i-1], self.str1[j-1])})
-    #                 i -= 1
-    #                 j -= 1
-
-    #     return self.back_trc
-
-
-
-
-
-
-
-
-
-            # if i > 0 and x == c + self.ins_cost:
-            #     add_operation("delete", [i-1, -1])
-            #     i -= 1
-
-            # elif j > 0 and x == b + self.del_cost:
-            #     add_operation("insert", [-1, j-1])
-            #     j -= 1
-
-            # else:  # Replace (Least favored)
-            #     add_operation("replace", [i-1, j-1])
-            #     i -= 1
-            #     j -= 1
