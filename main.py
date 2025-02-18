f4c0c3680307f8886886678b225e15300d1fb3cc
natsu <alaoui.hamza2002@gmail.com>
2025-02-18 01:25:00 +0100

    words sound compairing added

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from data import kyb_k, dict
 from min_edit_dist import min_dist
+import jellyfish
 
-source = "las"
+source = input("Type a french word: ")
 y = []
 
 for x in dict:
-    y.append(
-        {
-            x: (
-                min_dist(source, x, rep_cost=1).dist()
-                if x[0] == source[0] or x[0] in kyb_k[x[0]]
-                else min_dist(source, x).dist()
-            )
-        }
-    )
-y = sorted(y, key=lambda k: list(k.values()))
 
-t = 0
-for i in y:
-    print(i)
-    t += 1
-    if t == 30:
-        break
+    reverse_cost = round(jellyfish.jaro_winkler_similarity(source, x) / 2, 1)
+    if x[0] == source[0]:
+        reverse_cost += 0.3
+    elif x[0] in kyb_k[x[0]]:
+        reverse_cost += 0.2
+    elif len(x) == len(source):
+        reverse_cost += 0.4
+
+    y.append({x: (min_dist(source, x, rep_cost=2 - reverse_cost).dist())})
+
+
+y = sorted(y, key=lambda k: list(k.values()))
+if list(y[0].values())[0] == 0:
+    print("You typed it right, that's impressive!")
+else:
+    print("Broo, no way you're this bad at typing!\nLet me help you, did you mean:")
+
+    t = 0
+    for i in y:
+        print(i)
+        t += 1
+        if t == 15:
+            break
